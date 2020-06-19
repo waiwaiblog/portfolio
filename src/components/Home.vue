24df6ca75219a2e17f31a93ae30ba90a1317d02b
waiwaiblog <webmaster@waiwaiblog.net>
2020-06-19 14:55:18 +0900

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-parallax
+    light
+    src="@/assets/images/pic1.jpg"
+    size
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <h1 class="font1 mb-4">{{ title }}</h1>
+        <h4 class="font1">{{ subTitle }}</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        title: "waiwaiblog.net",
+        subTitle: "Welcome to my portfolio!",
+
+      }
+    }
+  }
+</script>
