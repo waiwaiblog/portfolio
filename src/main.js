7ef6cba16456a3d959686cd321efee54da22adc1
waiwaiblog <webmaster@waiwaiblog.net>
2020-07-17 04:52:41 +0900

    API pass削除

--- src/main.js
+++ src/main.js
@@ -8,14 +8,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 Vue.config.productionTip = false
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDKkd3ugxfaX3F6Cg8J8-2LaBl4Cia2KgY",
-  authDomain: "portfolio-waiwai-55092.firebaseapp.com",
-  databaseURL: "https://portfolio-waiwai-55092.firebaseio.com",
-  projectId: "portfolio-waiwai-55092",
-  storageBucket: "portfolio-waiwai-55092.appspot.com",
-  messagingSenderId: "163338673940",
-  appId: "1:163338673940:web:dc1f061847da31146d6c01",
-  measurementId: "G-0PWQ0Z7HFR"
+
 };
 
 firebase.initializeApp(firebaseConfig);
